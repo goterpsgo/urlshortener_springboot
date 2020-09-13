123c8321310c2a9b97e5a6b8c75e0fdd170ffaca
Joe Tseng <jtsengorg@gmail.com>
2020-09-12 22:19:27 -0400

    Added a new endpoint.

--- src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
+++ src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
@@ -9,4 +9,9 @@ public class UrlShortenerController {
     public String index() {
         return "Hello world";
     }
+
+    @RequestMapping("/bec")
+    public String bec() {
+        return "I ❤️ Bec";
+    }
 }
