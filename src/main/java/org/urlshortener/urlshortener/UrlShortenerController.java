8b52a6186d9e77912287167025d70fa8abc20725
Joe Tseng <jtsengorg@gmail.com>
2020-09-17 15:51:58 -0400

    [WIP] added thymeleaf template, finding out why it's not being used.

--- src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
+++ src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
@@ -1,13 +1,19 @@
 package org.urlshortener.urlshortener;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class UrlShortenerController {
     @RequestMapping("/")
-    public String index() {
+    public String hello_world() {
         return "Hello world";
+    }
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
     }
 
     @RequestMapping("/bec")
