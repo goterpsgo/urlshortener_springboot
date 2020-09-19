8367f6f0b980269f206846f689048dd247322275
Joe Tseng <jtsengorg@gmail.com>
2020-09-18 20:47:09 -0400

    [WIP] added new greeting template and endpoint.

--- src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
+++ src/main/java/org/urlshortener/urlshortener/UrlShortenerController.java
@@ -1,8 +1,11 @@
 package org.urlshortener.urlshortener;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class UrlShortenerController {
@@ -11,9 +14,15 @@ public class UrlShortenerController {
         return "Hello world";
     }
 
-    @RequestMapping(value = "/index")
+    @GetMapping(value = "/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
     @RequestMapping("/bec")
